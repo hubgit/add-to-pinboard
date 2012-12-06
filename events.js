d7904c9dcac22dca85aa16a8d28b742e0c1047fa
Alf Eaton <alf@hubmed.org>
2012-12-06 13:02:14 +0000

    add a keyboard shortcut

--- events.js
+++ events.js
@@ -1,10 +1,3 @@
-/* when the toolbar button is clicked, insert a script to extract data from the current tab */
-chrome.browserAction.onClicked.addListener(function() {
-	chrome.tabs.executeScript(null, { file: "extract.js" }, function(result) {
-		window.open("https://pinboard.in/add?" + build_params(result[0]), "Pinboard", "width=700,height=350");
-	});
-});
-
 /* helper script to encode URL parameters */
 var build_params = function(params) {
 	var items = [];
@@ -16,4 +9,22 @@ var build_params = function(params) {
 	}
 
 	return items.join("&");
-}
+};
+
+/* insert a script to extract data from the current tab */
+var save_to_pinboard = function() {
+	chrome.tabs.executeScript(null, { file: "extract.js" }, function(result) {
+		var url = "https://pinboard.in/add?" + build_params(result[0]);
+		window.open("https://pinboard.in/add?" + build_params(result[0]), "Pinboard", "width=700,height=350");
+		/* window.close doesn't close windows created by chrome.windows.create */
+		//chrome.windows.create({ url: url, type: "detached_panel", width: 700, height: 350, focused: true });
+	});
+};
+
+/* when the toolbar button is clicked */
+chrome.browserAction.onClicked.addListener(save_to_pinboard);
+
+/* when the keyboard shortcut is pressed */
+chrome.commands.onCommand.addListener(function(command) {
+  if (command == "save-to-pinboard") save_to_pinboard();
+});
