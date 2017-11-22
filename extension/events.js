8dbc9355b710f5705f8b84db535c50ad9c6099a0
Alf Eaton <eaton.alf@gmail.com>
2017-11-22 12:10:36 +0000

    ES6

--- extension/events.js
+++ extension/events.js
@@ -1,30 +1,23 @@
-/* helper script to encode URL parameters */
-var build_params = function(params) {
-	var items = [];
-
-	for (var param in params) {
-		if (params.hasOwnProperty(param) ) {
-			items.push(encodeURIComponent(param) + "=" + encodeURIComponent(params[param]));
-		}
-	}
-
-	return items.join("&");
-};
+const buildQueryString = params => Object.keys(params)
+	.map(key => [key, params[key]].map(encodeURIComponent).join('='))
+	.join('&')
 
 /* insert a script to extract data from the current tab */
-var save_to_pinboard = function() {
-	chrome.tabs.executeScript(null, { file: "extract.js" }, function(result) {
-		var url = "https://pinboard.in/add?" + build_params(result[0]);
-		window.open("https://pinboard.in/add?" + build_params(result[0]), "Pinboard", "width=700,height=350");
+const saveToPinboard = () => {
+	chrome.tabs.executeScript(null, { file: 'extract.js' }, ([item]) => {
+		const url = 'https://pinboard.in/add?' + buildQueryString(item)
+
+		window.open(url, 'Pinboard', 'width=700,height=350')
+
 		/* window.close doesn't close windows created by chrome.windows.create */
-		//chrome.windows.create({ url: url, type: "detached_panel", width: 700, height: 350, focused: true });
-	});
-};
+		//chrome.windows.create({ url: url, type: 'detached_panel', width: 700, height: 350, focused: true })
+	})
+}
 
 /* when the toolbar button is clicked */
-chrome.browserAction.onClicked.addListener(save_to_pinboard);
+chrome.browserAction.onClicked.addListener(saveToPinboard)
 
 /* when the keyboard shortcut is pressed */
-chrome.commands.onCommand.addListener(function(command) {
-  if (command == "save-to-pinboard") save_to_pinboard();
-});
+chrome.commands.onCommand.addListener(command => {
+  if (command === 'save-to-pinboard') saveToPinboard()
+})
--- extension/extract.js
+++ extension/extract.js
@@ -1,12 +1,12 @@
-var data = {
+const data = {
 	url: document.location.href,
 	title: document.title,
 	description: document.getSelection().toString()
-};
+}
 
 // add quotes around the selected text, if there is any
 if (data.description) {
-	data.description = '"' + data.description + '"';
+	data.description = '"' + data.description + '"'
 }
 
-data;
+data
